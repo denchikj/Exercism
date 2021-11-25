af8e9a77b5a58e0478864f40c4b8731221a89594
denchikj <igoshevdenis9876@gmail.com>
2021-11-25 12:25:15 +0500

    LogLevels

---
+++ LogLevels/LogLevels.fs
@@ -0,0 +1,11 @@
+﻿module LogLevels
+
+let message (logLine: string): string = logLine.Split(":").[1].Trim()
+
+let logLevel(logLine: string): string = 
+    match logLine.Trim().Split(" ")[0] with
+    | "[ERROR]:" -> "error"
+    | "[INFO]:" -> "info"
+    | _ -> "warning"
+
+let reformat(logLine: string): string = sprintf "%s (%s)" (message logLine) (logLevel logLine) 
