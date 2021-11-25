5e196b62d762fcd7b95f947e0f3d0e9f03e4dd3c
denchikj <igoshevdenis9876@gmail.com>
2021-11-25 12:23:05 +0500

    CarsAssemble

---
+++ CarsAssemble/CarsAssemble.fs
@@ -0,0 +1,15 @@
+﻿module CarsAssemble
+
+let successRate (speed: int): float =
+    match speed with 
+    | x when speed > 0 && speed < 5 -> 1.0
+    | x when speed > 4 && speed < 9 -> 0.9
+    | 9 -> 0.8
+    | 10 -> 0.77
+    | _ -> 0.0
+
+let productionRatePerHour (speed: int): float =
+    float (speed * 221) * successRate speed
+
+let workingItemsPerMinute (speed: int): int =
+    int (productionRatePerHour speed) / 60
