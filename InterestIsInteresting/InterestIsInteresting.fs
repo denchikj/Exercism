f5dc6617a77f982a38b12fc73e092f11cd59af3e
denchikj <igoshevdenis9876@gmail.com>
2021-11-29 21:32:28 +0500

    InterestIsInteresting

---
+++ InterestIsInteresting/InterestIsInteresting.fs
@@ -0,0 +1,19 @@
+﻿module InterestIsInteresting
+
+let interestRate (balance: decimal) : single =
+    match balance with
+    | x when x < 0m -> 3.213f
+    | x when x < 1000m -> 0.5f
+    | x when x >= 1000m && x < 5000m -> 1.621f
+    | _ -> 2.475f
+
+let interest (balance: decimal) : decimal =
+    balance
+    * decimal ((interestRate balance) / 100.0f)
+
+let annualBalanceUpdate (balance: decimal) : decimal = balance + interest balance
+
+let amountToDonate (balance: decimal) (taxFreePercentage: float) : int =
+    match balance with
+    | x when x < 0m -> 0
+    | _ -> int (balance * 2m * decimal (taxFreePercentage) / 100m)
