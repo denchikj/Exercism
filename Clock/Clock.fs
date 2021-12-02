8ed204eebf63192fc3c686b0b78578edbbe0c211
denchikj <igoshevdenis9876@gmail.com>
2021-12-02 14:20:04 +0500

    Clock

---
+++ Clock/Clock.fs
@@ -0,0 +1,10 @@
+﻿module Clock
+
+let create hours minutes =
+    (hours * 60 + minutes + 24 * 60 * 100) % (24 * 60)
+
+let add minutes clock = clock + minutes
+let subtract minutes clock = create 0 (clock - minutes)
+
+let display clock =
+    sprintf "%02i:%02i" (clock / 60 % 24) (clock % 60)
