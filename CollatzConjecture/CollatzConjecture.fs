fe8b7c3c278176ee40eb98af228ed77aba0f0920
denchikj <igoshevdenis9876@gmail.com>
2021-12-02 11:47:40 +0500

    CollatzConjecture

---
+++ CollatzConjecture/CollatzConjecture.fs
@@ -0,0 +1,11 @@
+﻿module CollatzConjecture
+
+let rec steps (number: int) : int option =
+    match number with
+    | 1 -> Some 0
+    | number when number < 1 -> None
+    | number when number % 2 = 0 -> number / 2 |> steps |> Option.map (fun x -> x + 1)
+    | number ->
+        3 * number + 1
+        |> steps
+        |> Option.map (fun x -> x + 1)
