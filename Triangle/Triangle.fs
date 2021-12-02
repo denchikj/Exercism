6ccae1155d2de0ccc827d42befd9e05e86944e8d
denchikj <igoshevdenis9876@gmail.com>
2021-12-02 13:51:03 +0500

    Triangle

---
+++ Triangle/Triangle.fs
@@ -0,0 +1,24 @@
+﻿module Triangle
+
+let check a b c =
+    a > 0.0
+    && b > 0.0
+    && c > 0.0
+    && (a + b) > c
+    && (b + c) > a
+    && (c + a) > b
+
+let equilateral triangle =
+    match triangle with
+    | [ a; b; c ] -> check a b c && a = b && b = c && c = a
+    | _ -> false
+
+let isosceles triangle =
+    match triangle with
+    | [ a; b; c ] -> check a b c && (a = b || b = c || c = a)
+    | _ -> false
+
+let scalene triangle =
+    match triangle with
+    | [ a; b; c ] -> check a b c && a <> b && b <> c && c <> a
+    | _ -> false
