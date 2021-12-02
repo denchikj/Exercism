dd60ac93e881f56a604c2eebfe96762a63e74dc7
denchikj <igoshevdenis9876@gmail.com>
2021-12-02 12:14:35 +0500

    Darts

---
+++ Darts/Darts.fs
@@ -0,0 +1,8 @@
+﻿module Darts
+
+let score (x: double) (y: double) : int =
+    match sqrt (x * x + y * y) with
+    | x when x <= 1.0 -> 10
+    | x when x <= 5.0 -> 5
+    | x when x <= 10.0 -> 1
+    | _ -> 0
