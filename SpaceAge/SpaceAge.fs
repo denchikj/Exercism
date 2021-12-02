9f78decdd463deb1e030abff3e65714b808bf5e0
denchikj <igoshevdenis9876@gmail.com>
2021-12-02 12:05:22 +0500

    SpaceAge

---
+++ SpaceAge/SpaceAge.fs
@@ -0,0 +1,33 @@
+﻿module SpaceAge
+
+open System
+
+type Planet =
+    | Mercury
+    | Venus
+    | Earth
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+let ratioToEarthYears planet =
+    match planet with
+    | Mercury -> 0.2408467
+    | Venus -> 0.61519726
+    | Earth -> 1.0
+    | Mars -> 1.8808158
+    | Jupiter -> 11.862615
+    | Saturn -> 29.447498
+    | Uranus -> 84.016846
+    | Neptune -> 164.79132
+
+
+let age (planet: Planet) (seconds: int64) : float =
+    Math.Round(
+        float seconds
+        / ratioToEarthYears planet
+        / 31557600.0,
+        2
+    )
