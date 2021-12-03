5f0180358b9b34f374a27d8150d6bc8a1ec6f4de
denchikj <igoshevdenis9876@gmail.com>
2021-12-03 09:37:01 +0500

    PerfectNumbers

---
+++ PerfectNumbers/PerfectNumbers.fs
@@ -0,0 +1,18 @@
+﻿module PerfectNumbers
+
+type Classification =
+    | Perfect
+    | Abundant
+    | Deficient
+
+let classify n : Classification option =
+    let sum =
+        [ 1 .. n - 1 ]
+        |> Seq.filter (fun x -> n % x = 0)
+        |> Seq.sum
+
+    match n with
+    | x when x > 0 && sum = n -> Some Perfect
+    | x when x > 0 && sum > n -> Some Abundant
+    | x when x > 0 && sum < n -> Some Deficient
+    | _ -> None
