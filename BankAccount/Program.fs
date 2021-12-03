16e5dd1d42dda4dc3e17964efbbed351b16c1788
denchikj <igoshevdenis9876@gmail.com>
2021-12-03 13:18:57 +0500

    BankAccount

---
+++ BankAccount/Program.fs
@@ -0,0 +1,27 @@
+﻿module BankAccount
+
+type Account =
+    { mutable Balance: decimal
+      mutable IsOpen: bool }
+
+let mkBankAccount () = { Balance = 0.0m; IsOpen = false }
+
+let openAccount account =
+    account.IsOpen <- true
+    account
+
+let closeAccount account =
+    account.IsOpen <- false
+    account
+
+let getBalance (account: Account) =
+    match account with
+    | { IsOpen = true } -> Some account.Balance
+    | _ -> None
+
+let updateBalance change account =
+    match account with
+    | { IsOpen = true } -> lock account (fun () -> account.Balance <- account.Balance + change)
+    | _ -> failwith "Account is closed"
+
+    account
