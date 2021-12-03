405398b67d48e27d680445e47346b3b246bd44bf
denchikj <igoshevdenis9876@gmail.com>
2021-12-03 12:09:52 +0500

    SpiralMatrix

---
+++ SpiralMatrix/SpiralMatrix.fs
@@ -0,0 +1,14 @@
+﻿module SpiralMatrix
+
+let spiralMatrix size =
+    let rec innerMatrix it sizes =
+        let rotate = List.transpose >> List.map List.rev
+        let x, z = sizes
+
+        match x with
+        | x when x = 0 && z = 0 -> []
+        | _ ->
+            [ it .. (it + x - 1) ]
+            :: (innerMatrix (it + x) (z - 1, x) |> rotate)
+
+    innerMatrix 1 (size, size)
