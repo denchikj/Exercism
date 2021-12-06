0045001d63ecee5ba1b41d4dce1088137aff26e6
denchikj <igoshevdenis9876@gmail.com>
2021-12-06 10:09:53 +0500

    Allergies

---
+++ Allergies/Allergies.fs
@@ -0,0 +1,21 @@
+﻿module Allergies
+
+open System
+
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
+
+let allergicTo (codedAllergies: int) (allergen: Allergen) =
+    codedAllergies &&& int allergen = int allergen
+
+let list codedAllergies =
+    Enum.GetValues<Allergen>()
+    |> Seq.filter (allergicTo codedAllergies)
+    |> Seq.toList
