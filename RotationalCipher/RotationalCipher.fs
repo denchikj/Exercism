28889eaf04bdcdc21f716cd4ceff2b57ea14b18d
denchikj <igoshevdenis9876@gmail.com>
2021-12-09 09:51:56 +0500

    RotationalCipher

---
+++ RotationalCipher/RotationalCipher.fs
@@ -0,0 +1,22 @@
+﻿module RotationalCipher
+
+open System
+
+let rotate shiftKey (text: string) =
+    match shiftKey with
+    | 0
+    | 26 -> text
+    | _ ->
+        String.map
+            (fun x ->
+                if Char.IsLetter x then
+                    let lastLetter = if Char.IsUpper x then 'Z' else 'z'
+                    let shifted = int x + shiftKey
+
+                    if int lastLetter >= shifted then
+                        char shifted
+                    else
+                        shifted - 26 |> char
+                else
+                    x)
+            text
