0d0b5728c15d4123e206cbaa5ff5dc20feb8eccb
denchikj <igoshevdenis9876@gmail.com>
2021-12-09 09:24:56 +0500

    RunLengthEncoding

---
+++ RunLengthEncoding/RunLengthEncoding.fs
@@ -0,0 +1,24 @@
+﻿module RunLengthEncoding
+
+open System
+open System.Text.RegularExpressions
+
+let encode (input: string) =
+    Regex.Matches(input, "(.)\1*")
+    |> Seq.map
+        (fun x ->
+            match x.Value.Length with
+            | 1 -> x.Groups.[1].Value
+            | count -> $"{count}{x.Groups.[1].Value}")
+    |> String.Concat
+
+let decode (input: string) =
+    Regex.Matches(input, "(\d*)(.)")
+    |> Seq.map
+        (fun x ->
+            match x.Groups.[1].Value with
+            | "" -> x.Groups.[2].Value
+            | num ->
+                Seq.init (int num) (fun _ -> x.Groups.[2].Value)
+                |> String.Concat)
+    |> String.Concat
