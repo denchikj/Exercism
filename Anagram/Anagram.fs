70ea8ef4083b8a5a8bca998bea506163a451665b
denchikj <igoshevdenis9876@gmail.com>
2021-12-09 09:41:12 +0500

    Anagram

---
+++ Anagram/Anagram.fs
@@ -0,0 +1,8 @@
+﻿module Anagram
+
+let findAnagrams (sources: string list) (target: string) =
+    let sort (s: string) = s.ToLower() |> Seq.sort |> Seq.toArray
+
+    sources
+    |> List.filter (fun x -> x.ToLower() <> target.ToLower())
+    |> List.filter (fun x -> sort x = sort target)
