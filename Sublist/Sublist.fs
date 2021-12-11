99bc3bd1405ed62b44068b88577105c8f4a867f2
denchikj <igoshevdenis9876@gmail.com>
2021-12-11 12:12:35 +0500

    Sublist

---
+++ Sublist/Sublist.fs
@@ -0,0 +1,25 @@
+﻿module Sublist
+
+type SublistType =
+    | Equal
+    | Sublist
+    | Superlist
+    | Unequal
+
+let rec sublistOf xs ys =
+    match (xs, ys) with
+    | ([], _) -> true
+    | (x :: xtail, y :: ytail) ->
+        if x = y
+           && xtail = (List.truncate (List.length xtail) ytail) then
+            true
+        else
+            sublistOf xs ytail
+    | _ -> false
+
+let sublist xs ys =
+    match (sublistOf xs ys, sublistOf ys xs) with
+    | (true, true) -> Equal
+    | (true, false) -> Sublist
+    | (false, true) -> Superlist
+    | (false, false) -> Unequal
